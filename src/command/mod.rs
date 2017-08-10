c6d9a621096a1f1ba8067e4382c3bba1d0008aac
PoiScript <poiscript@gmail.com>
2017-08-10 08:55:06 +0800

    chore: rewrite

--- src/command/mod.rs
+++
@@ -1,6 +0,0 @@
-use adapter::Message;
-
-pub trait Command: Send + Sync {
-    fn handle(&self, msg: Message) -> Option<Box<Message>>;
-    fn match_command(&self, command: String) -> bool;
-}
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,20 @@
-extern crate hyper;
-extern crate hyper_native_tls;
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate nom;
 extern crate serde;
+extern crate hyper;
+extern crate hyper_tls;
+extern crate env_logger;
 extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
-#[macro_use]
-extern crate log;
-extern crate env_logger;
-#[macro_use]
-extern crate nom;
 
-mod adapter;
-mod command;
-mod sagiri;
-
-use sagiri::Sagiri;
-use adapter::matrix::MatrixAdapter;
-use adapter::telegram::TelegramAdapter;
+mod bot;
+mod error;
+mod kitsu;
+mod database;
 
 fn main() {
-    //    let args: Vec<String> = std::env::args().collect();
-    //
-    //    env_logger::init().unwrap();
-    //
-    //    if args.len() != 2 {
-    //        error!("Usage: {} CONFIG_FILE", args[0]);
-    //        std::process::exit(1);
-    //    }
-    //
-    //    print!("Sagiri Here");
-    //
-    //    let sagiri: Sagiri;
+  println!("Sagiri Here.")
 }
